c6031704dcb002e6b3c95d309b01f33fc3f2ff11
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-22 01:16:39 +0800

    add: problem 35

---
+++ Algorithms/35. search-insert-position/search-insert-position.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        // binary search
+        while (left <= right) {
+            // prevent overflow
+            int mid = left + (right - left) / 2;
+
+            // search left part if target is larger
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
+// @lc code=end
