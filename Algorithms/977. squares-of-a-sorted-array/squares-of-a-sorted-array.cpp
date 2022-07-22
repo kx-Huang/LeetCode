7d6c21f9e1b4bdec268b56c67f2a6a606a1aed18
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-22 17:16:09 +0800

    add: problem 977

---
+++ Algorithms/977. squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=977 lang=cpp
+ *
+ * [977] Squares of a Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        // two pointers
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        int left = 0;
+        int right = n - 1;
+
+        // compare and sqaure from two sides to middle
+        for (int k = n - 1; k >= 0; k--) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                ans[k] = pow(nums[left], 2);
+                left++;
+            } else {
+                ans[k] = pow(nums[right], 2);
+                right--;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
