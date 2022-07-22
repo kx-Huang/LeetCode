5c34f34d6fa1b51cb31ede681d3a01aef07dab33
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-22 16:59:13 +0800

    add: problem 475

---
+++ Algorithms/475. heaters/heaters.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=475 lang=cpp
+ *
+ * [475] Heaters
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        // sort houses and heaters: O(nlogn+mlogm)
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        int radius = 0;
+        // two pointers: O(n+m)
+        for (int i = 0, j = 0; i < houses.size(); i++) {
+            // move to next heater if next one is closer
+            while (j + 1 < heaters.size() && abs(heaters[j + 1] - houses[i]) <=
+                                                 abs(heaters[j] - houses[i]))
+                j++;
+            radius = max(abs(heaters[j] - houses[i]), radius);
+        }
+        return radius;
+    }
+};
+// @lc code=end
