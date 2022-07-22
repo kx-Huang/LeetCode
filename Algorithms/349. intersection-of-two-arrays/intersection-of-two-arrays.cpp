016fe932a08cb6c5386c926bc1528573feaef99e
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-22 19:46:25 +0800

    add: problem 349

---
+++ Algorithms/349. intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=349 lang=cpp
+ *
+ * [349] Intersection of Two Arrays
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        vector<int> ans;
+
+        // if an element found in set:
+        //  1. delete element to prevent double count
+        //  2. add to answer vector
+        for (int e : nums2)
+            if (s.erase(e)) ans.push_back(e);
+
+        return ans;
+    }
+};
+// @lc code=end
