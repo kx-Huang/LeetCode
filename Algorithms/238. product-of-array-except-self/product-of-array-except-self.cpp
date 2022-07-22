d55f94dca4ad60f527f6a8a5b4b56cb9ee25062d
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-22 11:59:15 +0800

    add: problem 238

---
+++ Algorithms/238. product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=238 lang=cpp
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        int productFromLeft = 1;
+        int productFromRight = 1;
+        vector<int> ans(n, 1);
+
+        // keep left and right cumulative product
+        for (int i = 0; i < n; i++) {
+            ans[i] *= productFromLeft;
+            productFromLeft *= nums[i];
+            ans[n - 1 - i] *= productFromRight;
+            productFromRight *= nums[n - 1 - i];
+        }
+        return ans;
+    }
+};
+// @lc code=end
