8e09e57f272331c350695d6c7903d1ea2bfcce6a
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-24 19:43:15 +0800

    update: problem 46

--- Algorithms/46. permutations/permutations.cpp
+++ Algorithms/46. permutations/permutations.cpp
@@ -2,6 +2,10 @@
  * @lc app=leetcode id=46 lang=cpp
  *
  * [46] Permutations
+ *
+ * Methods: Backtracking
+ * Data Structure: Array
+ *
  */
 
 // @lc code=start
@@ -9,20 +13,20 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        backtrack(ans, nums, 0);
+        backtrack(ans, nums, 0);  // O(n*n!)
         return ans;
     }
 
 private:
-    // Backtracking
     void backtrack(vector<vector<int>>& ans, vector<int>& nums, int pos) {
         if (pos == nums.size() - 1) {
             ans.push_back(nums);
             return;
         }
+        // recursion: O(n*n!)
         for (int i = pos; i < nums.size(); i++) {
             swap(nums[pos], nums[i]);
-            backtrack(ans, nums, pos + 1);
+            backtrack(ans, nums, pos + 1);  // O(n!)
             swap(nums[pos], nums[i]);
         }
     }
