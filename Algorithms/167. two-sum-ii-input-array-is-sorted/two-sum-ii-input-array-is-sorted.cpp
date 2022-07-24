408e4668ba200e43b9b63082115223118220e08d
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-24 19:43:04 +0800

    update: problem 167

--- Algorithms/167. two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ Algorithms/167. two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -2,17 +2,20 @@
  * @lc app=leetcode id=167 lang=cpp
  *
  * [167] Two Sum II - Input Array Is Sorted
+ *
+ * Methods: Two Pointers
+ * Data Structure: Array
+ *
  */
 
 // @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        // two pointers method
         int left = 0;
         int right = numbers.size() - 1;
 
-        // compare the sum and shrink the range
+        // compare the sum and shrink the range: O(n)
         while (left < right) {
             int sum = numbers[left] + numbers[right];
             if (sum == target)
