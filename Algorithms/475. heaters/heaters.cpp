e8573d39de220fcf9776b09cfc5bad23c8b6e201
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-24 19:44:11 +0800

    update: problem 475

--- Algorithms/475. heaters/heaters.cpp
+++ Algorithms/475. heaters/heaters.cpp
@@ -2,6 +2,10 @@
  * @lc app=leetcode id=475 lang=cpp
  *
  * [475] Heaters
+ *
+ * Methods: Two Pointers
+ * Data Structure: Array
+ *
  */
 
 // @lc code=start
@@ -12,7 +16,8 @@ public:
         sort(houses.begin(), houses.end());
         sort(heaters.begin(), heaters.end());
         int radius = 0;
-        // two pointers: O(n+m)
+
+        // Two pointers: O(n+m)
         for (int i = 0, j = 0; i < houses.size(); i++) {
             // move to next heater if next one is closer
             while (j + 1 < heaters.size() && abs(heaters[j + 1] - houses[i]) <=
