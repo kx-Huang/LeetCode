0daf958c7a0324e866fe8fc242df084ff3eb1ec6
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-24 19:43:47 +0800

    update: problem 217

--- Algorithms/217. contains-duplicate/contains-duplicate.cpp
+++ Algorithms/217. contains-duplicate/contains-duplicate.cpp
@@ -2,19 +2,24 @@
  * @lc app=leetcode id=217 lang=cpp
  *
  * [217] Contains Duplicate
+ *
+ * Methods:
+ * Data Structure: Hash Table
+ *
  */
 
 // @lc code=start
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // Solution 1: check if any insertion to set fails
         unordered_set<int> s;
+
+        // Solution 1: check if any insertion to set fails: O(n)
         for (int i = 0; i < nums.size(); i++)
             if (s.insert(nums[i]).second == false) return true;
         return false;
 
-        // Solution 2: Compare size after constructing set
+        // Solution 2: Compare size after constructing set // O(n)
         // return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();
     }
 };
