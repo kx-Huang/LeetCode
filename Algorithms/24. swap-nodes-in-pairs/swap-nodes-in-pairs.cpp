2e6654c5267f777fb4723076725c184db856843d
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-30 18:02:29 +0800

    add: problem 24

---
+++ Algorithms/24. swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=24 lang=cpp
+ *
+ * [24] Swap Nodes in Pairs
+ *
+ * Methods: Traversal with Iteration
+ * Data Structure: Linked List
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* newHead = new ListNode();
+        ListNode* prev = newHead;
+        ListNode* curr = head;
+
+        while (1) {
+            if (curr->next == NULL) {  // one node left, append and return
+                prev->next = curr;
+                return newHead->next;
+            } else {  // have 2 nodes
+                ListNode* next = curr->next->next;
+                // append 2 nodes in reverse order
+                prev->next = curr->next;
+                prev = prev->next;
+                prev->next = curr;
+                prev = prev->next;
+                prev->next = NULL;
+                if (next == NULL) return newHead->next;  // return if no pair
+                curr = next;
+            }
+        }
+        // code should not reach here
+        assert(false);
+    }
+};
+// @lc code=end
