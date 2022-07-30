7c62920b1b1c0ecd1add36b3bdd70ed7d59ff009
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-30 18:25:25 +0800

    add: problem 86

---
+++ Algorithms/86. partition-list/partition-list.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=86 lang=cpp
+ *
+ * [86] Partition List
+ *
+ * Methods: Traversal with Iteration
+ * Data Structure: Linked List
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* smallDummyHead = new ListNode();  // dummy head
+        ListNode* largeDummyHead = new ListNode();  // dummy head
+        ListNode* small = smallDummyHead;
+        ListNode* large = largeDummyHead;
+
+        // append small value to small list, large value to large list
+        while (head != NULL) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+
+        // append large to small
+        small->next = largeDummyHead->next;
+        large->next = NULL;
+
+        return smallDummyHead->next;
+    }
+};
+// @lc code=end
