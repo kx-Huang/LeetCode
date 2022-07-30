aeb80cc022cac462918da9cf65b3e64b2ad97335
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-31 02:15:14 +0800

    add: problem 160

---
+++ Algorithms/160. intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=160 lang=cpp
+ *
+ * [160] Intersection of Two Linked Lists
+ *
+ * Methods: Traversal with Iteration
+ * Data Structure: Linked List
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // A = a + common
+        // B = b + common
+        // (a + common +   b) + common
+        // (b   + common + a) + common
+        //                    ^
+        //                stop here
+        ListNode *nodeA = headA, *nodeB = headB;
+        while (nodeA != nodeB) {
+            nodeA = nodeA ? nodeA->next : headB;
+            nodeB = nodeB ? nodeB->next : headA;
+        }
+        return nodeA;
+    }
+};
+// @lc code=end
