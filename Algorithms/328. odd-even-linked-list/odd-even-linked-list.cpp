69a2413d5ef5a895216f366b26d2e428806e68c5
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-31 17:23:30 +0800

    add: problem 328

---
+++ Algorithms/328. odd-even-linked-list/odd-even-linked-list.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ *
+ * Methods: Traversal with Iteration
+ * Data Structure: Linked List
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL || head->next == NULL || head->next->next == NULL)
+            return head;
+
+        ListNode* oddNode = head;
+        ListNode* evenHead = head->next;
+        ListNode* evenNode = evenHead;
+
+        // move forward two nodes at a time
+        // odd -> (even -> odd)
+        while (evenNode && evenNode->next) {
+            oddNode->next = evenNode->next;
+            oddNode = oddNode->next;
+            evenNode->next = oddNode->next;
+            evenNode = evenNode->next;
+        }
+        // append even list to odd list
+        oddNode->next = evenHead;
+        return head;
+    }
+};
+// @lc code=end
