090bef5a00f4c82584e04a8f1b06cf28fb1abe0c
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-09 02:20:21 +0800

    add: problem 148

---
+++ Algorithms/148. sort-list/sort-list.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=148 lang=cpp
+ *
+ * [148] Sort List
+ *
+ * Methods: Divide and Conquer
+ * Data Structure: Linked List
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        // Slow & Fast Pointers
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // divide into two lists
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = slow->next;
+        slow->next = NULL;
+
+        // recursion merge the divided lists
+        return mergeTwoLists(sortList(firstHalf), sortList(secondHalf));
+    }
+
+private:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummyHead = ListNode();
+        ListNode* cur = &dummyHead;
+
+        // traverse two lists and append smaller node
+        for (; l1 && l2; cur = cur->next) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+        }
+
+        // append the remaining list to the merged list
+        if (l1)
+            cur->next = l1;
+        else
+            cur->next = l2;
+
+        return dummyHead.next;
+    }
+};
+// @lc code=end
