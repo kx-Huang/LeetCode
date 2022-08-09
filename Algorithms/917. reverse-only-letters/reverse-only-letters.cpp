5399095570bc0ffdf395838272dd48298a61c86e
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-09 22:29:54 +0800

    add: problem 917

---
+++ Algorithms/917. reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=917 lang=cpp
+ *
+ * [917] Reverse Only Letters
+ *
+ * Methods: Two Pointers
+ * Data Structure: String
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        // Two Pointers: O(n)
+        while (left < right) {
+            if (isalpha(s[left]) == 0)
+                left++;
+            else if (isalpha(s[right]) == 0)
+                right--;
+            else
+                swap(s[left++], s[right--]);
+        }
+        return s;
+    }
+};
+// @lc code=end
