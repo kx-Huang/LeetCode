b6b706f9d1783d90f85882ec5436e980ba02f964
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-11 22:56:28 +0800

    add: problem 20

---
+++ Algorithms/20. valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * Methods: Iterative Traversal
+ * Data Structure: String, Stack
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() == 1) return false;
+
+        stack<char> st;
+
+        // store open brackets in stack and match top with close brackets: O(n)
+        for (char c : s) {
+            if (c == '(' | c == '{' | c == '[') {
+                st.push(c);
+            } else {
+                // no match
+                if (st.empty()) return false;
+                // wrong match
+                if (c == ')' && st.top() != '(') return false;
+                if (c == '}' && st.top() != '{') return false;
+                if (c == ']' && st.top() != '[') return false;
+                // right match
+                st.pop();
+            }
+        }
+
+        // empty stack means all match, or exists miss match
+        return st.empty();
+    }
+};
+// @lc code=end
