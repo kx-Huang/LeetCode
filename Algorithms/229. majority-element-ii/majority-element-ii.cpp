4973e7bc777f8fd7d20b6af4031c1d486014866b
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-14 04:32:48 +0800

    update: problem 229

--- Algorithms/229. majority-element-ii/majority-element-ii.cpp
+++ Algorithms/229. majority-element-ii/majority-element-ii.cpp
@@ -12,8 +12,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
+        int len = nums.size();
+        if (len <= 1) return nums;
+
+        int candidateA = INT_MAX;
+        int candidateB = INT_MAX;
+        int countA = 0;
+        int countB = 0;
+
+        // Boyer–Moore majority vote algorithm: O(n)
+        for (int c : nums) {
+            if (c == candidateA) {
+                countA++;
+            } else if (c == candidateB) {
+                countB++;
+            } else if (countA == 0) {
+                candidateA = c;
+                countA = 1;
+            } else if (countB == 0) {
+                candidateB = c;
+                countB = 1;
+            } else {
+                countA--;
+                countB--;
+            }
+        }
+
+        // count vote for candidate A and B again to determine results: O(n)
+        vector<int> ans;
+        if (count(nums.begin(), nums.end(), candidateA) > len / 3)
+            ans.push_back(candidateA);
+        if (count(nums.begin(), nums.end(), candidateB) > len / 3)
+            ans.push_back(candidateB);
+        return ans;
     }
 };
 // @lc code=end
-
