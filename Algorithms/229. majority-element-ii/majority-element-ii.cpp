643074d5f0ccf8832b8fc2256a1bb6f39840c0f4
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-14 03:39:06 +0800

    add: problem 229

---
+++ Algorithms/229. majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=229 lang=cpp
+ *
+ * [229] Majority Element II
+ *
+ * Method: Boyer–Moore Majority Vote Algorithm
+ * Data Structure: Array
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
