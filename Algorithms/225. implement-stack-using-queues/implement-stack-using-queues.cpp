f732ac8646c59ac0a2edddef57dd7364d6ca1112
kx-huang <Huang.KX@sjtu.edu.cn>
2022-08-18 15:51:21 +0800

    add: problem 225

---
+++ Algorithms/225. implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=225 lang=cpp
+ *
+ * [225] Implement Stack using Queues
+ */
+
+// @lc code=start
+class MyStack {
+private:
+    queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {}
+
+    int pop() {}
+
+    int top() {}
+
+    bool empty() {}
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
