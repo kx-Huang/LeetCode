4df2e79ae3e86323ff3fc22b08dc24e6be9ab982
kx-huang <Huang.KX@sjtu.edu.cn>
2022-11-08 04:56:44 -0500

    update: problem 33

--- Algorithms/33. search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ Algorithms/33. search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -20,46 +20,27 @@ public:
         int right = len - 1;
 
         // binary search: O(logn)
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
+            // find match
             if (nums[mid] == target) return mid;
 
-            // left < mid < right: normal case
-            if (nums[left] < nums[right]) {
-                if (target > nums[mid])
+            // see 33.png for illustration
+            // determine target relationships by comparing left and middle point
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
                 else
-                    right = mid;
-            }
-            // right < left < mid: turn-over in right range
-            //      left <= target <= mid   left
-            //      mid < target            right
-            //      target <= right         right
-            //      right < target < left   impossible
-            else if (nums[mid] >= nums[left]) {
-                if (target <= nums[mid] && target >= nums[left])
-                    right = mid;
-                else if (target > nums[mid] || target <= nums[right])
-                    left = mid + 1;
-                else
-                    return -1;
-            }
-            // mid < right < left: turn-over in left range
-            //      mid <= target <= right  right
-            //      target < mid            left
-            //      left <= target          left
-            //      right < target < left   impossible
-            else if (nums[mid] < nums[right]) {
-                if (target <= nums[right] && target >= nums[mid])
-                    left = mid + 1;
-                else if (target >= nums[left] || target < nums[mid])
-                    right = mid;
-                else
-                    return -1;
+                    right = mid - 1;
             }
         }
-
-        return nums[left] == target ? left : -1;
+        // no match found after binary search
+        return -1;
     }
 };
 // @lc code=end
