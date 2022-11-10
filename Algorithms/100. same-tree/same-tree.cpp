cd892f15548e374a94d464da6ceb73a2ba90e6fd
kx-huang <Huang.KX@sjtu.edu.cn>
2022-11-10 01:31:19 -0500

    add: problem 100

---
+++ Algorithms/100. same-tree/same-tree.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=100 lang=cpp
+ *
+ * [100] Same Tree
+ *
+ * Method: Divide and Conquer
+ * Data Structure: Binary Tree
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+// @lc code=end
